d4c841d06d77acbd6bd8860570474fc00a840638
FarrelGhozy <farrelafif05@gmail.com>
2026-04-21 13:17:17 +0700

    memperbaiki port
    memperbaiki port agar tidak terjadi masalah dengan layanan yang ada di
    server

--- backend/services/whatsappService.js
+++ backend/services/whatsappService.js
@@ -7,7 +7,7 @@ class WhatsAppService {
   constructor() {
     // URL WAHA di dalam network docker adalah http://waha:3000
     // Jika diakses dari luar docker (saat development) gunakan http://localhost:3000
-    this.baseURL = process.env.WAHA_URL || 'http://waha:3000';
+    this.baseURL = process.env.WAHA_URL || 'http://waha:8000';
     this.session = process.env.WAHA_SESSION || 'default';
     this.apiKey = process.env.WAHA_API_KEY || 'adminutc28';
   }
