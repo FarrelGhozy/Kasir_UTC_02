89b331b314189da6793d14750e104224fddd2ff1
FarrelGhozy <farrelafif05@gmail.com>
2026-04-23 21:35:47 +0700

    pq

--- backend/bot/botConfig.js
+++ backend/bot/botConfig.js
@@ -11,5 +11,5 @@ module.exports = {
     CLOSE: 15 // 15.00 WIB
   },
   CLOSED_DAY: 5, // 5 = Jumat (0 = Minggu, 6 = Sabtu)
-  SESSION_LIMIT: 12 * 60 * 60 * 1000, // 12 Jam dalam milidetik
+  SESSION_LIMIT: 1 * 60 * 60 * 1000, // 1 Jam dalam milidetik
 };
