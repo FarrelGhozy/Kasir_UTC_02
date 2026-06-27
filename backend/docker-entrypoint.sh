656a2cd9ea0646b506fd52eaa1de1d92a0adef5a
FarrelGhozy <farrelafif05@gmail.com>
2026-06-27 18:33:03 +0700

    perbaikan yang baru masuk ignore

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Buat folder uploads & backups dengan permission yang benar (volume mount mungkin override ownership)
-mkdir -p /app/uploads/services /app/uploads/items /app/backups
+mkdir -p /app/uploads/services /app/uploads/items /app/uploads/notas /app/backups
 chmod -R 777 /app/uploads
 chmod -R 777 /app/backups
 
